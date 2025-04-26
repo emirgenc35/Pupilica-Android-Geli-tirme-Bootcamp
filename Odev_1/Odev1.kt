9e4d93c2b7628f910ba31a0019c776393bed65a9
Emir Genç <gencemir7@gmail.com>
2025-04-26 15:16:11 +0300

    Create Odev1.kt
    Android Geliştirme Bootcamp'inde yaptığım projeler

---
+++ Odev_1/Odev1.kt
@@ -0,0 +1,146 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main(){
+    var sehir = "izmir"
+    println("sehir: $sehir")
+
+    var ulke = "Turkiye"
+    println("ulke: $ulke")
+
+    var telefon = 12345678900
+    println("telefon: $telefon")
+
+    var posta_kod = 123456
+    println("posta_kod: $posta_kod")
+
+    var email = "example@gmail.com"
+    println("email: $email")
+
+    var meslek = "Yazilimci"
+    println("meslek: $meslek")
+
+    var stok_miktar = 100
+    println("stok_miktar: $stok_miktar")
+
+    var musteri_ad = "Ali"
+    println("musteri_ad: $musteri_ad")
+
+    var bakiye = 1000
+    println("bakiye: $bakiye")
+
+    var dogum_gun = "26.04.2025"
+    println("dogum_gun: $dogum_gun")
+
+    var cinsiyet = "erkek"
+    println("cinsiyet: $cinsiyet")
+
+    var maas = 30000
+    println("maas: $maas")
+
+    var medeni_durum = "bekar"
+    println("medeni_durum: $medeni_durum")
+
+    var evli_mi = true
+    println("evli_mi: $evli_mi")
+
+    var urun_yorum = "guzel"
+    println("urun_yorum: $urun_yorum")
+
+    var odeme_tarih = "26.04.2025"
+    println("odeme_tarih: $odeme_tarih")
+
+    var odeme = true
+    println("odeme: $odeme")
+
+    var siparis_adet = 5
+    println("siparis_adet: $siparis_adet")
+
+    var araba_modeli = "Subaru"
+    println("araba_modeli: $araba_modeli")
+
+    var kitap_ad = "Martin Eden"
+    println("kitap_ad: $kitap_ad")
+    var yayin_tarihi = "26.04.2025"
+    println("yayin tarihi : $yayin_tarihi")
+
+    var indirim_miktar = 10
+    println("indirim_miktar: $indirim_miktar")
+    var oda_sayi = 4
+    println("oda_sayi: $oda_sayi")
+
+    var enlem = 38.56
+    println("enlem: $enlem")
+
+    var boylam = 32.67
+    println("boylam: $boylam")
+
+    var urun_ad = "klavye"
+    println("urun_ad: $urun_ad")
+
+    var yemek_fiyat = 350
+    println("yemek_fiyat: $yemek_fiyat")
+
+    var marka = "monster"
+    println("marka : $marka")
+
+    var muzik_ad = "yalniz"
+    println("muzik adi : $muzik_ad")
+
+    var video_sure = 360 // saniye
+    println("video suresi : $video_sure")
+
+    var urun_puan = 2
+    println("urun puani : $urun_puan")
+
+    var resim_ad = "hamburger.png"
+    println("resim adi : $resim_ad")
+
+    var dosya_formati = "kullanici.csv"
+    println("dosya formati : $dosya_formati")
+
+    var renk = "Beyaz"
+    println("Renk : $renk")
+
+    var renk_kodu = "#FFFFFF"
+    println("Renk kodu : $renk_kodu")
+
+    var telefon_model = "Samsung S23 Ultra"
+    println("Telefon modeli : $telefon_model")
+
+    var ekran_boyut = 6.8
+    println("Ekran boyutu : $ekran_boyut")
+
+    var agirlik = 86.7
+    println("Agirlik : $agirlik")
+
+    var ulusal_gun = "29 Ekim Cumhuriyet Bayrami"
+    println("Ulusal gun : $ulusal_gun")
+
+    var tatil_gun = "1 Mayis"
+    println("Tatil gunu : $tatil_gun")
+
+    var rezervasyon_tarih = "26.04.2025"
+    println("Rezervasyon tarihi : $rezervasyon_tarih")
+
+    var sokak_ad = "Firin Sokak"
+    println("Sokak adi : $sokak_ad")
+
+    var otobus_hat ="90"
+    println("Otobus hat : $otobus_hat")
+
+    var kalan_dakika = 300
+    println("kalan dakika : $kalan_dakika")
+
+    var takip_kod = 11111
+    println("takip kod : $takip_kod")
+
+    var kupon_sure = 360
+    println("kupon suresi : $kupon_sure")
+
+    var kupon_kodu = "KUPON123"
+    println("kupon kodu : $kupon_kodu")
+
+    var fatura_adresi = "Firin Sokak Cicek apt. no:1 kat:1 Istanbul"
+    println("fatura adresi : $fatura_adresi")
+
+}
